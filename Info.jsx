cd1e0e8f48e95d0114f6e9e4aa5b05f87000d357
Devanshi631 <78401528+Devanshi631@users.noreply.github.com>
2021-06-26 20:20:44 +0530

    Add files via upload

---
+++ Info.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Info() {
+  return (
+    <div className="note">
+      <h1>JavaScript and React.js</h1>
+      <p>
+        A basic Web Development React js bootcamp conducted by ShapeAI with
+        instructor Mr. Shaurya Sinha
+      </p>
+    </div>
+  );
+}
+
+export default Info;
